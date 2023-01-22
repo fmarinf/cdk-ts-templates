ed6b9fe92542425f5ab45897adfe4eb046276c6a
Fernando Marín Figueroa <102365693+fmarinf@users.noreply.github.com>
2023-01-22 09:42:35 -0300

    reformat file

--- plantillas-cdk/template-streaming.ts
+++ plantillas-cdk/template-streaming.ts
@@ -15,31 +15,30 @@ const stream = new kinesis.Stream(stack, 'MyStream', {
 
 // Crear un Kinesis Data Analytics para analizar datos del flujo
 const analytics = new kinesisAnalytics.CfnApplication(stack, 'MyAnalytics', {
-    // Configuraciones del Kinesis Data Analytics
-  });
-  
-  // Crear una función Lambda para fanout los datos
-  const fanoutLambda = new lambda.Function(stack, 'FanoutLambda', {
-    // Configuraciones de la función Lambda
-  });
-  
-  // Crear un topic SNS para publicar los datos
-  const topic = new sns.Topic(stack, 'MyTopic');
-  
-  // Crear una tabla DynamoDB para almacenar los datos
-  const table = new dynamoDB.Table(stack, 'MyTable', {
-    // Configuraciones de la tabla DynamoDB
-  });
-  
-  // Conectar el Data Stream con el Analytics
-  analytics.connectToStream(stream);
-  
-  // Conectar la salida del Analytics con la función Lambda
-  analytics.connectToLambda(fanoutLambda);
-  
-  // Conectar la salida de la función Lambda con el topic SNS y la tabla DynamoDB
-  fanoutLambda.addEventSource(new lambda.SnsEventSource(topic));
-  fanoutLambda.addEventSource(new lambda.DynamoEventSource(table, {
-    startingPosition: lambda.StartingPosition.TRIM_HORIZON
-  }));
-  
+  // Configuraciones del Kinesis Data Analytics
+});
+
+// Crear una función Lambda para fanout los datos
+const fanoutLambda = new lambda.Function(stack, 'FanoutLambda', {
+  // Configuraciones de la función Lambda
+});
+
+// Crear un topic SNS para publicar los datos
+const topic = new sns.Topic(stack, 'MyTopic');
+
+// Crear una tabla DynamoDB para almacenar los datos
+const table = new dynamoDB.Table(stack, 'MyTable', {
+  // Configuraciones de la tabla DynamoDB
+});
+
+// Conectar el Data Stream con el Analytics
+analytics.connectToStream(stream);
+
+// Conectar la salida del Analytics con la función Lambda
+analytics.connectToLambda(fanoutLambda);
+
+// Conectar la salida de la función Lambda con el topic SNS y la tabla DynamoDB
+fanoutLambda.addEventSource(new lambda.SnsEventSource(topic));
+fanoutLambda.addEventSource(new lambda.DynamoEventSource(table, {
+  startingPosition: lambda.StartingPosition.TRIM_HORIZON
+}));
